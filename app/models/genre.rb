811749697ea1d574c49f552519da772888e693c4
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 15:50:11 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -18,7 +18,7 @@ class Genre < ActiveRecord::Base
     
     # return an array of strings containing every musician's name
      self.songs.collect do |song|
-      song.artist 
-  end
+      song.artist.name
+  end.uniq
   end
 end
